f4249530074c8be008e67a5ffe13a55420d79a31
sunflower-mue <cmneswiswa@gmail.com>
2023-08-10 03:48:40 -0700

    betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 	{
-		char q [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		
+		char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 		write(1, q, 59);
 		return (1);
 	}
