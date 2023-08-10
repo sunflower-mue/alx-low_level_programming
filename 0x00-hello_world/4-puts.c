ad56e66a466b3b4c2e44474a338990b625959cc1
sunflower-mue <cmneswiswa@gmail.com>
2023-08-10 02:43:44 -0700

    paranthesis on return statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *Return -  uses puts to print out the message.
  *0 Indicates succesful execution.
  */
- 
+
 int main(void)
 	{
 		puts("\"Programming is like building a multilingual puzzle");
-		return 0;
+		return(0);
 	}
