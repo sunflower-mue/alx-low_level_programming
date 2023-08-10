2c4d594212f9cb44ccee6d858bd621fbc0290b5a
sunflower-mue <cmneswiswa@gmail.com>
2023-08-10 02:59:03 -0700

    added 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - programm entry point.
+ * Description: uses printf to print a statement.
+ * Return: 0 Indicates successful exec.
+ */
+
+int main(void)
+	{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+	}
