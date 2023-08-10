e55da94ab904eebbd5c8c26584ef59b8dff23083
sunflower-mue <cmneswiswa@gmail.com>
2023-08-10 02:36:35 -0700

    betty style used

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main is our entry point.
+ * Return -  uses puts to print out the message.
+ * 0 Indicates succesful execution.
+ */
+ 
 int main() {
-	puts("Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
+	
 	return 0;
 }
