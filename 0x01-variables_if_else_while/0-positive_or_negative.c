5c77ac4421d837b9b1c96e5fa115d4dc87d93c30
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 03:18:18 -0700

    0 added descr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,9 @@
 
 /**
  * main() - prints an integer n and tells if it's neg, pos or zero.
- * Description: < 0, > 0 or == 0.
+ *
+ * Description: conditions of the main func < 0, > 0 or == 0.
+ *
  * Return: 0 for success.
  */
 
