4bc5157f55bed82ff62ab8cb2a7b5eed011ea0bd
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 04:36:19 -0700

    added 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main prints all base 16 digits but lower case a-f.
+ * base 16 digits are 0-9,A-F. A total of 16 digits.
+ *base16 'a'+1 = b.
+ * Return: 0 for succesful operation.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchat('0' + i);
+		}
+
+		else
+		{
+			putchar('a' + i - 10);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
