7c1f10a6590a915738e5ef2f396625a700c29714
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 05:01:47 -0700

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,11 @@ int main(void)
 {
 	int i;
 	
-	putchar('0');
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(',');
-		putchar(' ');
 		putchar('0' + i);
+		putchar(',');
+		putchar(' ')
 	}
 	return (0);
 }
