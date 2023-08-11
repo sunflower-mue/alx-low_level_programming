4ea96c8e648f59dce9d240d5a45d73b6aa7a2144
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 03:45:20 -0700

    not betty style

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,11 @@
 
 int main(void)
 {
-	char letter;
-	
+	char letter;	
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	for (letter = 'A'; letter <= 'Z'; letter++)
-		putchar(letter);
-	
+		putchar(letter);	
 	putchar('\n');
 	return (0);
 }
