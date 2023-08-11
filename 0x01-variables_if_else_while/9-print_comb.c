b79e730fc7c93dd63d131497f2717abc7d481d4d
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 04:52:18 -0700

    added 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints combination of single digits 0-9.
+ * It must be in ascending order.
+ * Use only putchar a max of 4 times.
+ * No char var.
+ *
+ * Return: 0 for a successful exec.
+ */
+
+int main(void)
+{
+	int i;
+	
+	putchar('0');
+	for (i = 1; i < 10; i++)
+	{
+		putchar(',');
+		putchar(' ')
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
