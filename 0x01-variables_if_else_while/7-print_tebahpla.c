338ed3636803b2d8d33b7c0509a64914db33c6e7
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 04:00:33 -0700

    added 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse orde.
+ * Return: 0 for successful execution.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar("\n");
+	
+	return (0);
+}
