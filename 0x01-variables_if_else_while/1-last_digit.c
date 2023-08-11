003f6543c0f9b501e40ea5b5a1d0d4a86f7c5f7d
sunflower-mue <cmneswiswa@gmail.com>
2023-08-11 03:26:07 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@
 int main(void)
 	{
 		int n;
+		int lastDigit;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-
-		int lastDigit = n % 10;
+		lastDigit = n % 10;
 
 		printf("Last digit of %d is %n ", n, lastDigit);
 
@@ -24,11 +24,11 @@ int main(void)
 		{
 			printf("and is greater than 5\n",n, lastDigit);
 			}
-		else if (lastDigit < 6 && lastDigit != 0)
+		if (lastDigit < 6 && lastDigit != 0)
 		{
 			printf("and is less than 6 and not 6\n",n,lastDigit);
 			}
-		else
+		if (lastDigit == 0)
 		{
 			printf("and is zero");
 		}
