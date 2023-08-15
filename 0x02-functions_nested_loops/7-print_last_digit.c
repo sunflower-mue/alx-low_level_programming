c75ebb8b029a44a07b3ab045369589b4319411b1
sunflower-mue <cmneswiswa@gmail.com>
2023-08-15 13:52:24 -0700

    updated main.h and added 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_last_digit - prints last digit of int.
+ *@n integer param.
+ *Return: last digit.
+ */
+
+int print_last_digit(int n)
+{
+	int lasDigit = n % 10;
+
+	return (lastDigit);
+}
