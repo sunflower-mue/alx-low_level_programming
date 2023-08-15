aa22d2a430bf15e482d7cfadd952338812bf2325
sunflower-mue <cmneswiswa@gmail.com>
2023-08-15 14:01:15 -0700

    modulo last int only for pos int

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,19 @@
 
 /**
  *print_last_digit - prints last digit of int.
- *@n integer param.
+ *@n: integer param.
  *Return: last digit.
  */
 
 int print_last_digit(int n)
 {
-	int lastDigit = n % 10;
+	int lastDigit;
 
+	if (n < 0)
+		lastDigit = -1*(n % 10);
+	else
+		lastDigit = n % 10;
+
+	_putchar(lastDigit + '0');
 	return (lastDigit);
 }
