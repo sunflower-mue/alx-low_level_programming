e78ff189faf07a5e4a7a35e4a5f6df2af11853b2
sunflower-mue <cmneswiswa@gmail.com>
2023-08-15 14:58:36 -0700

    added 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - prints all ints from n to 98.
+ *@n: input or param of type int.
+ */
+
+void print_to_98(int n)
+{
+	int i, j;
+
+	if (n <= 98)
+	{
+
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+
+	else
+	{
+		for (j = n; j >=98; j--)
+		{
+			printf("%d",j);
+			if ( j !=98)
+				printf(", ");
+		}
+	}
+
+	printf("\n");
+}
