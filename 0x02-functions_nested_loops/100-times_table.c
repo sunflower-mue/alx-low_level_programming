f409a0b104926e90e16d3d6bf5076c14e5bc5f25
sunflower-mue <cmneswiswa@gmail.com>
2023-08-15 16:18:24 -0700

    100 re-written

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,45 @@
 #include "main.h"
 
 /**
- *print_times_table - prints n times table from 0.
- *if n > 15 || n < 0, print nothing.
- *@n: our func param of type int.
+ *print_times_table - print n table from 0.
+ *@n: param dimension
  */
-
 void print_times_table(int n)
 {
-	int i, j, mul;
+	int i, j, k;
 
-	if (n <= 15 || n >= 0)
+	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar(48);
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				mul = i * j;
-
-				if (mul <= 9)
-					_putchar(' ');
-				if (mul <= 99)
-					_putchar(' ');
-				if (mul >= 100)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar((mul / 100) + 48);
-					_putchar((mul / 10) % 10 +48);
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
-				else if (mul <= 99 && mul >= 10)
-					_putchar((mul / 10) + 48);
-				_putchar((mul % 10) + 48);
 			}
 			_putchar('\n');
 		}
