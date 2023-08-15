1338ac8812a64cfbcb9666d792467a5847904ae6
sunflower-mue <cmneswiswa@gmail.com>
2023-08-15 13:41:52 -0700

    added 6 and upd header

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - absolute value of an int.
+ *@n: integer parameter.
+ *Return:-n or n0 for success.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+	{
+		return (n);
+	}
+}
