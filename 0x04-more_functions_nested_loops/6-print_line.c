04789b49bafda39b18750394fd40e51dbbec8f1c
sunflower-mue <cmneswiswa@gmail.com>
2023-08-17 11:56:13 -0700

    re did 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,16 @@
 #include "main.h"
 
 /**
- *print_line - printing a straight line.
- *
- *@n: param of int type. 
- *specifies how many times _ should be printed
- *Return: void for nothing.
+*print_line - printing a straight line.
+*@n:parameter of type int.
+*Return:returns nothing, void type.
 */
 
 void print_line(int n)
 {
-	int i;
-
-	if (n <= 0)
-		_putchar('\n');
-	else
+	while (n-- > 0)
 	{
-		for (i = 1; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
