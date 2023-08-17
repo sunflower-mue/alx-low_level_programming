3262a764d962c70210af58ee7aadc80abf5946ae
sunflower-mue <cmneswiswa@gmail.com>
2023-08-17 05:13:32 -0700

    adding 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_diagonal - printing a diagonal line.
+ * @n: param of type int. Number of char \ is printed.
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i j++)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
+		}
+	}
+}
