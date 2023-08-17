668839d850fd7a9be60bdc85a6b9db73f5df1106
sunflower-mue <cmneswiswa@gmail.com>
2023-08-17 12:37:37 -0700

    added 7 again

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,28 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
-*print_diagonal - prints diagonal.
-*@n: param of type int
-*Return: returns nothing.
-*/
-
+ * print_diagonal - depending on n, print diagonal.
+ * @n : param of type int of lines using '\' characters to use
+ * Return: No return type.
+ */
 void print_diagonal(int n)
 {
-	int i, j;
-	
-	if (n > 0)
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 0; j < i; j++)
-				_putchar(' ');
-			_putchar('\\');
-			
-			if (i == (n - 1))
-				continue;
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
