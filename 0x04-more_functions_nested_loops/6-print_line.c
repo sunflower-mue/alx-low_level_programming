cdac177b9b8d2ee70b60820d48aac61cc40f5c0a
sunflower-mue <cmneswiswa@gmail.com>
2023-08-17 03:14:52 -0700

    added 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ *print_line - printing a straight line.
+ *
+ * @n: param of int type. 
+ * specifies how many times _ should be printed
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
