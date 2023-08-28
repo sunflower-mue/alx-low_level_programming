3eb2ac991b592e6e22cee9669dbf2bcb7e41e1a5
sunflower-mue <cmneswiswa@gmail.com>
2023-08-28 10:48:20 -0700

    pointer is of type char0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
  * _memset - fills memory with a contanst byte.
- * @s: paramete pointer to an int.
+ * @s: parameter pointer to a char.
  * @b: parameter of type char.
  * @n: unsigned int param.
  *
  * Return: A pointer to the memory area s.
  */
 
-char *_memset(int *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = b; /*remember string is an arr*/
+		s[i] = b;
 	}
 	return (s);
 }
