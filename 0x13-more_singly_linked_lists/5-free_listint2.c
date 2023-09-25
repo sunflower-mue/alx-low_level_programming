3235ef1d34d43d4ac96637100d57d568a02e5417
sunflower-mue <cmneswiswa@gmail.com>
2023-09-25 07:47:50 -0700

    free2 double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ * function sets the head to NULL.
+ * @head: A pointer to a pointer to head of LL.
+ * Return: Nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *tmp;
+
+	while (current)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+}
+
+
