05f6d4f92e4909b919e5e0b1b97eec88c51b890a
sunflower-mue <cmneswiswa@gmail.com>
2023-09-25 10:11:35 -0700

    removed comments

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,19 +12,17 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	/*If no linked lis- NUll*/
 	if (head == NULL)
 		return (NULL);
 
 	listint_t *new_node = malloc(sizeof(listint_t));
-	/*if failed to malloc*/
-	while (!new_node)
+
+	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
 	if (idx == 0)
 	{
-		/*point to were head points*/
 		new_node->next = *head;
 		*head = new_node;
 
