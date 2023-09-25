e49a775a6a9839c1e3f7eea09f71499b5957f203
sunflower-mue <cmneswiswa@gmail.com>
2023-09-25 10:31:32 -0700

    10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of ll.
+ * @head: double pointer to head of ll.
+ * @index: index of node to delete.
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp = NULL;
+	listint_t *current = *head;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+		*head = (*head)->next;
+		free(current);
+		return (1);
+
+	while (i < index - 1)
+	{
+		if (!current || !(current->next))
+			return (-1);
+
+		current = currrent->next;
+		i++;
+	}
+
+	tmp = current->next;
+	current->next = tmp->next;
+	free(tmp);
+
+	return (1);
+
+}
