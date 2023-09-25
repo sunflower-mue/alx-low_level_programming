0bf13e80379e000c5ace8754e9cdd3e7348d35d0
sunflower-mue <cmneswiswa@gmail.com>
2023-09-25 10:36:00 -0700

    brackets on if?

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,9 +17,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	if (index == 0)
+	{
 		*head = (*head)->next;
 		free(current);
 		return (1);
+	}
 
 	while (i < index - 1)
 	{
