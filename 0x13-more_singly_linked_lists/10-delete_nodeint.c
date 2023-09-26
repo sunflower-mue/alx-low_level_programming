1c11a334f8d6daeedd7b1c6f9163c64ff3ded322
sunflower-mue <cmneswiswa@gmail.com>
2023-09-26 11:12:01 -0700

    current not currrent

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (!current || !(current->next))
 			return (-1);
 
-		current = currrent->next;
+		current = current->next;
 		i++;
 	}
 
