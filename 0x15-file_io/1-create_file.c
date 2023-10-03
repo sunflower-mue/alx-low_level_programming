53a348c5c32ff31362fa1472029fa900f325b340
sunflower-mue <cmneswiswa@gmail.com>
2023-10-03 05:09:05 -0700

    strlen - 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		written = write(fd, text_content, strlen(text_content));
+		written = write(fd, text_content, strlen(text_content) - 1);
 		if (written == -1)
 		{
 			close(fd);
