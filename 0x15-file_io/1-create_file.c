ecbf33bd4604c5110d68c8d5527af3bbf779dc6c
sunflower-mue <cmneswiswa@gmail.com>
2023-10-03 07:11:11 -0700

    perm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,14 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		written = write(fd, text_content, strlen(text_content) - 1);
+		written = write(fd, text_content, strlen(text_content));
 		if (written == -1)
 		{
 			close(fd);
