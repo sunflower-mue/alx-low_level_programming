f0ad6a9b4ac9b35c0c022ee0720bc1954c4c74a5
sunflower-mue <cmneswiswa@gmail.com>
2023-10-03 05:02:32 -0700

    rdwr perm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 
 	if (fd == -1)
 		return (-1);
